3ee154ade78e8a106a9bd78a979059d3aba22c74
Dmitry Shagurov <shagurovda@gmail.com>
2024-03-03 14:03:23 +0400

    Add reserve button to the About section

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -2,6 +2,7 @@
 import classNames from "classnames";
 
 import { SectionContent } from "components/SectionContent";
+import { ReserveButton } from "components/ReserveButton";
 
 import styles from "./About.module.sass";
 
@@ -14,6 +15,7 @@ const About = () => {
         <div className={classNames(styles.column, styles.info)}>
           <h1>О нас</h1>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus rerum, enim laudantium consectetur sunt, neque est, quidem modi labore nisi ab? Dolorum, tenetur autem molestiae officia eos facilis itaque corrupti quisquam, rem mollitia inventore dolore nam explicabo amet quis cumque architecto dolores ab! Quisquam temporibus asperiores numquam voluptatibus. Error quam consectetur aliquam pariatur voluptate, inventore reiciendis, praesentium quo esse tempore debitis, animi beatae accusantium! Corrupti eum praesentium accusamus sunt voluptatum quia ab qui provident odio consequuntur quaerat beatae expedita odit, placeat repellat veritatis perferendis omnis sit et nisi iure? Dolorem, explicabo. In tempora nobis excepturi voluptatem? Error cumque possimus illum.</p>
+          <ReserveButton />
         </div>
 
         <div className={classNames(styles.column, styles.imageContainer)}></div>
