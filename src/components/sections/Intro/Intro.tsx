89c6e40753b776259f36efb0f6b4f6717b9aa6d3
Dmitry Shagurov <shagurovda@gmail.com>
2024-03-03 11:36:36 +0400

    Add styles for the Intro section

--- src/components/sections/Intro/Intro.tsx
+++ src/components/sections/Intro/Intro.tsx
@@ -1,11 +1,13 @@
 import { ReserveButton } from "components/ReserveButton";
 import { CAFE_NAME } from "constants/cafe";
 
+import styles from "./Intro.module.sass";
+
 
 const Intro = () => {
   return (
-    <section>
-      <div>{CAFE_NAME}</div>
+    <section className={styles.Intro}>
+      <div className={styles.label}>{CAFE_NAME}</div>
       <ReserveButton />
     </section>
   );
