4d9b5d8eff1747796fdeda347d12d0fd3178e2b2
Dmitry Shagurov <shagurovda@gmail.com>
2024-03-08 13:18:11 +0400

    Add MenuPreview config

---
+++ src/constants/menu.ts
@@ -0,0 +1,64 @@
+import { MenuItem } from "types";
+
+
+const menu: MenuItem[] = [
+  {
+    id: 1,
+    label: "Блюдо 1",
+    compound: "Граммовки 1",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 2,
+    label: "Блюдо 2",
+    compound: "Граммовки 2",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 3,
+    label: "Блюдо 3",
+    compound: "Граммовки 3",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 4,
+    label: "Блюдо 4",
+    compound: "Граммовки 4",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 5,
+    label: "Блюдо 5",
+    compound: "Граммовки 5",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 6,
+    label: "Блюдо 6",
+    compound: "Граммовки 6",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+  {
+    id: 7,
+    label: "Блюдо 7",
+    compound: "Граммовки 7",
+    info: "asddsdsadasdsa",
+    price: 249,
+    staticPreviewPath: "1.png",
+  },
+];
+
+
+export default menu;
