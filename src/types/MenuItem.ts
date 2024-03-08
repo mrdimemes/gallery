937e83aeebabd5d6f66862b2a52392876f2b43d8
Dmitry Shagurov <shagurovda@gmail.com>
2024-03-08 13:17:19 +0400

    Add the MenuItem global type

---
+++ src/types/MenuItem.ts
@@ -0,0 +1,11 @@
+type MenuItem = {
+  id: number,
+  label: string,
+  compound: string,
+  info: string | null,
+  price: number,
+  staticPreviewPath: string,
+};
+
+
+export default MenuItem;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export type { default as ComponentProps } from "./ComponentProps";
+export type { default as MenuItem } from "./MenuItem";
